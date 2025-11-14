4a7593cb9135b9f9afebd4eff07d9933aa24228b
motasimdev <themotasimdev@gmail.com>
2025-11-14 21:44:16 +0600

    fond added

--- fonts/Font.jsx
+++ fonts/Font.jsx
@@ -1,7 +1,12 @@
 import {Montserrat } from 'next/font/google';
+import {Orbitron } from 'next/font/google';
  
 
 export const montserrat = Montserrat({
+  weight: ['400', '700', '500'],
+  subsets: ['latin'],
+});
+export const orbitron = Orbitron({
   weight: ['400', '700', '500'],
   subsets: ['latin'],
 });
